b8e41ffecc5385be30c6e5f4e508ec623adc994b
zahari <zahari@polydraw.com>
2015-08-02 20:09:43 +0300

    Grayscale image for the window example with pseudo-random generator

--- examples/window.rs
+++ examples/window.rs
@@ -1,16 +1,37 @@
 #![cfg(target_os = "linux")]
 
 extern crate polydraw;
-extern crate rand;
 
 use std::iter::repeat;
-use rand::Rng;
 
 use polydraw::os::xcb;
 use polydraw::os::x11;
 use polydraw::os::egl;
 use polydraw::os::gl;
 use polydraw::os::cl;
+
+fn rand_u8(seed: &mut u64) -> u8 {
+    *seed = seed.wrapping_mul(58321).wrapping_add(11113);
+    (seed.wrapping_shr(16) % 256) as u8
+}
+
+fn create_data(width: usize, height: usize) -> Vec<u8> {
+   repeat(0u8)
+      .take(width * height * 3)
+      .collect::<Vec<_>>()
+}
+
+fn update_data(data: &mut Vec<u8>, width: usize, height: usize, seed: &mut u64) {
+   for y in 0..height {
+      for x in 0..width {
+         let i = 3 * (x + y * width);
+         let r = rand_u8(seed);
+         data[i] = r;
+         data[i + 1] = r;
+         data[i + 2] = r;
+      }
+   }
+}
 
 fn print_screen_info(screen: &xcb::Screen) {
    println!("Informations of screen .... : {}", screen.root());
@@ -162,11 +183,9 @@ fn main() {
 
    gl::reset_pixelstore_alignment();
 
-   let mut data = repeat(0u8)
-      .take(width * height * 3)
-      .collect::<Vec<_>>();
-
-   rand::thread_rng().fill_bytes(&mut data);
+   let mut seed: u64 = 0;
+   let mut data = create_data(width, height);
+   update_data(&mut data, width, height, &mut seed);
 
    let texture = gl::create_texture(width, height, &data);
 
@@ -186,15 +205,21 @@ fn main() {
 
       let event_type = event.event_type();
 
+      unsafe {
+         println!(
+            "XCB Event                   : R {}  S {}  F {}",
+            (*event.ptr).response_type,
+            (*event.ptr).sequence,
+            (*event.ptr).full_sequence
+         );
+      }
+
       match event_type {
          xcb::EventType::KeyPress => {
             break;
          },
          xcb::EventType::Expose => {
-            //gl::clear_color(0.0, 0.7, 1.0, 1.0);
-            //gl::clear();
-
-            //gl::blit_framebuffer(framebuffer, width, height);
+            gl::blit_framebuffer(framebuffer, width, height);
 
             gl::flush();
 
