b5d4666629d4e8d6d7870d6e05e438c48ff4e8ca
zahari <zahari@polydraw.com>
2015-09-07 01:19:55 +0300

    Removing old examples

--- examples/generic.rs
+++ examples/generic.rs
@@ -1,10 +1,23 @@
 extern crate polydraw;
 
-mod support;
-
 use polydraw::{Application, Renderer, RenderFrame};
 
-use support::update_data;
+pub fn rand_u8(seed: &mut u64) -> u8 {
+    *seed = seed.wrapping_mul(58321).wrapping_add(11113);
+    (seed.wrapping_shr(16) % 256) as u8
+}
+
+pub fn update_data(data: &mut Vec<u8>, width: u32, height: u32, seed: &mut u64) {
+   for y in 0..height {
+      for x in 0..width {
+         let i: usize = (3 * (x + y * width)) as usize;
+         let r = rand_u8(seed);
+         data[i] = r;
+         data[i + 1] = r;
+         data[i + 2] = r;
+      }
+   }
+}
 
 struct NoiseRenderer {
    counter: u64,
