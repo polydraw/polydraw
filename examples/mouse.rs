236561bb62d78bb0e89fa05256588e3f69bff40c
zahari <zahari@polydraw.com>
2015-09-27 18:15:23 +0300

    Simplifying the mouse example

--- examples/mouse.rs
+++ examples/mouse.rs
@@ -7,63 +7,24 @@ use polydraw::{Application, Renderer, RenderFrame};
 struct MouseRenderer {
    mouse_x: u32,
    mouse_y: u32,
-   old: Option<(u32, u32)>,
 }
 
 impl MouseRenderer {
    fn new() -> Self {
       MouseRenderer {
-         mouse_x: 0,
-         mouse_y: 0,
-         old: None,
+         mouse_x: 1000000,
+         mouse_y: 1000000,
       }
    }
 }
 
 impl Renderer for MouseRenderer {
    fn render(&mut self, frame: &mut RenderFrame) {
-      let half = 35_u32;
-
-      match self.old {
-         Some((old_x, old_y)) => {
-            let old_x_start = max(0, (old_x as i32) - half as i32) as u32;
-            let old_x_end = min(frame.width, old_x + half);
-
-            let old_y_start = max(0, (old_y as i32) - half as i32) as u32;
-            let old_y_end = min(frame.height, old_y + half);
-
-            for y in old_y_start..old_y_end {
-               let row_i = 3 * y * frame.width;
-
-               let mut i = (3 * old_x_start + row_i) as usize;
-               frame.data[i] = 33;
-               frame.data[i + 1] = 168;
-               frame.data[i + 2] = 222;
-
-               i = (3 * old_x_end + row_i) as usize;
-               frame.data[i] = 33;
-               frame.data[i + 1] = 168;
-               frame.data[i + 2] = 222;
-            }
-
-            for x in old_x_start..old_x_end {
-               let mut row_i = 3 * old_y_start * frame.width;
-
-               let mut i = (3 * x + row_i) as usize;
-               frame.data[i] = 33;
-               frame.data[i + 1] = 168;
-               frame.data[i + 2] = 222;
-
-               row_i = 3 * old_y_end * frame.width;
-
-               i = (3 * x + row_i) as usize;
-               frame.data[i] = 33;
-               frame.data[i + 1] = 168;
-               frame.data[i + 2] = 222;
-            }
-         },
-         None => {},
+      for i in 0 as usize..(frame.width * frame.height * 3) as usize {
+         frame.data[i] = 0;
       }
+
+      let half = 35_u32;
 
       let x_start = max(0, (self.mouse_x as i32) - half as i32) as u32;
       let x_end = min(frame.width, self.mouse_x + half);
@@ -80,38 +41,6 @@ impl Renderer for MouseRenderer {
             frame.data[i + 2] = 222;
          }
       }
-
-      for y in y_start..y_end {
-         let row_i = 3 * y * frame.width;
-
-         let mut i = (3 * x_start + row_i) as usize;
-         frame.data[i] = 255;
-         frame.data[i + 1] = 255;
-         frame.data[i + 2] = 255;
-
-         i = (3 * x_end + row_i) as usize;
-         frame.data[i] = 255;
-         frame.data[i + 1] = 255;
-         frame.data[i + 2] = 255;
-      }
-
-      for x in x_start..x_end {
-         let mut row_i = 3 * y_start * frame.width;
-
-         let mut i = (3 * x + row_i) as usize;
-         frame.data[i] = 255;
-         frame.data[i + 1] = 255;
-         frame.data[i + 2] = 255;
-
-         row_i = 3 * y_end * frame.width;
-
-         i = (3 * x + row_i) as usize;
-         frame.data[i] = 255;
-         frame.data[i + 1] = 255;
-         frame.data[i + 2] = 255;
-      }
-
-      self.old = Some((self.mouse_x, self.mouse_y));
    }
 
    fn mouse_moved(&mut self, x: u32, y: u32) {
