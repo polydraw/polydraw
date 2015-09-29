38813f68d971ada337fceb0797e4a6f26e3940c3
zahari <zahari@polydraw.com>
2015-09-29 23:51:33 +0300

    Non-public access for the Line and LineSegment member variables + update methods

--- examples/intersection.rs
+++ examples/intersection.rs
@@ -2,7 +2,6 @@ extern crate polydraw;
 
 use polydraw::{Application, Renderer, RenderFrame};
 use polydraw::geom::line::{LineSegment, LineIntersection};
-use polydraw::geom::point::Point;
 use polydraw::draw::{RGB, bresenham, hline, vline};
 
 struct IntersectionRenderer {
@@ -14,12 +13,12 @@ impl IntersectionRenderer {
    fn new() -> Self {
       IntersectionRenderer {
          l1: LineSegment::new(
-            Point::new(0_f64, 0_f64),
-            Point::new(0_f64, 0_f64)
+            0_f64, 0_f64,
+            0_f64, 0_f64
          ),
          l2: LineSegment::new(
-            Point::new(0_f64, 0_f64),
-            Point::new(0_f64, 0_f64)
+            0_f64, 0_f64,
+            0_f64, 0_f64
          )
       }
    }
@@ -41,17 +40,15 @@ impl IntersectionRenderer {
 
 impl Renderer for IntersectionRenderer {
    fn init(&mut self, frame: &RenderFrame) {
-      self.l1.p1.x = 100_f64;
-      self.l1.p1.y = 120_f64;
+      self.l1.update(
+         100_f64, 120_f64,
+         frame.width as f64 - 100_f64, frame.height as f64 - 100_f64
+      );
 
-      self.l1.p2.x = frame.width as f64 - 100_f64;
-      self.l1.p2.y = frame.height as f64 - 100_f64;
-
-      self.l2.p1.x = 140_f64;
-      self.l2.p1.y = frame.height as f64 - 90_f64;
-
-      self.l2.p2.x = frame.width as f64 - 140_f64;
-      self.l2.p2.y = 100_f64;
+      self.l2.update(
+         140_f64, frame.height as f64 - 90_f64,
+         frame.width as f64 - 140_f64, 100_f64
+      );
    }
 
    fn render(&mut self, frame: &mut RenderFrame) {
@@ -61,17 +58,17 @@ impl Renderer for IntersectionRenderer {
       let l2_color = RGB::new(127, 255, 223);
       let intersection_color = RGB::new(255, 223, 191);
 
-      let l1_p1_x = self.l1.p1.x as i32;
-      let l1_p1_y = self.l1.p1.y as i32;
+      let l1_p1_x = self.l1.x1() as i32;
+      let l1_p1_y = self.l1.y1() as i32;
 
-      let l1_p2_x = self.l1.p2.x as i32;
-      let l1_p2_y = self.l1.p2.y as i32;
+      let l1_p2_x = self.l1.x2() as i32;
+      let l1_p2_y = self.l1.y2() as i32;
 
-      let l2_p1_x = self.l2.p1.x as i32;
-      let l2_p1_y = self.l2.p1.y as i32;
+      let l2_p1_x = self.l2.x1() as i32;
+      let l2_p1_y = self.l2.y1() as i32;
 
-      let l2_p2_x = self.l2.p2.x as i32;
-      let l2_p2_y = self.l2.p2.y as i32;
+      let l2_p2_x = self.l2.x2() as i32;
+      let l2_p2_y = self.l2.y2() as i32;
 
       bresenham(frame, l1_p1_x, l1_p1_y, l1_p2_x, l1_p2_y, &l1_color);
       bresenham(frame, l2_p1_x, l2_p1_y, l2_p2_x, l2_p2_y, &l2_color);
@@ -81,7 +78,7 @@ impl Renderer for IntersectionRenderer {
       self.point_rect(frame, l2_p1_x, l2_p1_y, &l2_color);
       self.point_rect(frame, l2_p2_x, l2_p2_y, &l2_color);
 
-      let intersection = self.l1.line.intersect(&self.l2.line);
+      let intersection = self.l1.line().intersect(self.l2.line());
       match intersection {
          LineIntersection::Point(p) => {
             self.point_rect(frame, p.x as i32, p.y as i32, &intersection_color);
--- src/geom/line.rs
+++ src/geom/line.rs
@@ -9,21 +9,34 @@ pub enum LineIntersection<T> {
 
 #[derive(Debug)]
 pub struct Line<T> {
-   pub a: T,
-   pub b: T,
-   pub c: T
+   a: T,
+   b: T,
+   c: T
 }
 
 impl<T> Line<T> where T: Number {
-   pub fn new(p1: &Point<T>, p2: &Point<T>) -> Self {
-      let a = p2.y - p1.y;
-      let b = p1.x - p2.x;
-      let c = -(p1.x * p2.y - p2.x * p1.y);
+   pub fn new(x1: T, y1: T, x2: T, y2: T) -> Self {
+      let (a, b, c) = Self::calc_abc(x1, y1, x2, y2);
       Line {
          a: a,
          b: b,
          c: c
       }
+   }
+
+   pub fn update(&mut self, x1: T, y1: T, x2: T, y2: T) {
+      let (a, b, c) = Self::calc_abc(x1, y1, x2, y2);
+      self.a = a;
+      self.b = b;
+      self.c = c;
+   }
+
+   #[inline]
+   fn calc_abc(x1: T, y1: T, x2: T, y2: T) -> (T, T, T) {
+      let a = y2 - y1;
+      let b = x1 - x2;
+      let c = -(x1 * y2 - x2 * y1);
+      (a, b, c)
    }
 
    pub fn intersect(&self, line: &Self) -> LineIntersection<T> {
@@ -42,14 +55,16 @@ impl<T> Line<T> where T: Number {
 
 #[derive(Debug)]
 pub struct LineSegment<T> {
-   pub p1: Point<T>,
-   pub p2: Point<T>,
-   pub line: Line<T>
+   p1: Point<T>,
+   p2: Point<T>,
+   line: Line<T>
 }
 
 impl<T> LineSegment<T> where T: Number {
-   pub fn new(p1: Point<T>, p2: Point<T>) -> Self {
-      let line = Line::new(&p1, &p2);
+   pub fn new(x1: T, y1: T, x2: T, y2: T) -> Self {
+      let p1 = Point::new(x1, y1);
+      let p2 = Point::new(x2, y2);
+      let line = Line::new(x1, y1, x2, y2);
 
       LineSegment {
          p1: p1,
@@ -57,18 +72,53 @@ impl<T> LineSegment<T> where T: Number {
          line: line
       }
    }
+
+   pub fn update(&mut self, x1: T, y1: T, x2: T, y2: T) {
+      self.p1.x = x1;
+      self.p1.y = y1;
+      self.p2.x = x2;
+      self.p2.y = y2;
+
+      self.line.update(x1, y1, x2, y2);
+   }
+
+   #[inline]
+   pub fn x1(&self) -> T {
+      self.p1.x
+   }
+
+   #[inline]
+   pub fn y1(&self) -> T {
+      self.p1.y
+   }
+
+   #[inline]
+   pub fn x2(&self) -> T {
+      self.p2.x
+   }
+
+   #[inline]
+   pub fn y2(&self) -> T {
+      self.p2.y
+   }
+
+   #[inline]
+   pub fn line(&self) -> &Line<T> {
+      &self.line
+   }
 }
 
 #[cfg(test)]
 mod tests {
-   use super::LineSegment;
-   use super::super::point::Point;
+   use test::{Bencher, black_box};
+
+   use super::*;
 
    #[test]
    fn test_new_f64() {
       let line = LineSegment::new(
-         Point::new(10_f64, 20_f64),
-         Point::new(-100_f64, 0_f64)
+         10_f64, 20_f64,
+         -100_f64, 0_f64
       );
       assert_eq!(line.p1.x, 10_f64);
       assert_eq!(line.p1.y, 20_f64);
@@ -79,12 +129,23 @@ mod tests {
    #[test]
    fn test_new_f32() {
       let line = LineSegment::new(
-         Point::new(10_f32, 20_f32),
-         Point::new(-100_f32, 0_f32)
+         10_f32, 20_f32,
+         -100_f32, 0_f32
       );
       assert_eq!(line.p1.x, 10_f32);
       assert_eq!(line.p1.y, 20_f32);
       assert_eq!(line.p2.x, -100_f32);
       assert_eq!(line.p2.y, 0_f32);
+   }
+
+   #[bench]
+   fn bench_line_new_f64(b: &mut Bencher) {
+      b.iter(|| {
+         for _ in 0..1000 {
+            black_box(
+               Line::new(5_f64, 7_f64, 3_f64, 2_f64)
+            );
+         }
+      });
    }
 }
