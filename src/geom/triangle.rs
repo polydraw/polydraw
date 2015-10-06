8b3101b02d6ac4fc51b119eb054ee9b4187a0ed2
zahari <zahari@polydraw.com>
2015-10-06 17:06:16 +0300

    Changing the triangle member names to a, b and c

--- src/geom/triangle.rs
+++ src/geom/triangle.rs
@@ -2,17 +2,17 @@ use super::point::Point;
 
 #[derive(Debug)]
 pub struct Triangle {
-   pub v0: Point,
-   pub v1: Point,
-   pub v2: Point
+   pub a: Point,
+   pub b: Point,
+   pub c: Point
 }
 
 impl Triangle {
-   pub fn new(v0: Point, v1: Point, v2: Point) -> Self {
+   pub fn new(a: Point, b: Point, c: Point) -> Self {
       Triangle {
-         v0: v0,
-         v1: v1,
-         v2: v2
+         a: a,
+         b: b,
+         c: c
       }
    }
 }
