beb65c01f159b0554b5928bf1f59c067ae1a12ae
zahari <zahari@polydraw.com>
2015-10-13 19:03:40 +0300

    Horizontal and vertical splitting of polygons

--- src/geom/clip.rs
+++ src/geom/clip.rs
@@ -11,7 +11,7 @@ pub fn h_down_intersect(y: i64, p1: Point, p2: Point, points: &mut Vec<Point>) {
       if p1.y > y && p2.y < y {
          points.push(Point::new(h_intersect(y, p1, p2), y));
       }
-      points.push(Point::new(p2.x, p2.y));
+      points.push(p2);
    }
 }
 
@@ -25,7 +25,7 @@ pub fn h_up_intersect(y: i64, p1: Point, p2: Point, points: &mut Vec<Point>) {
       if p1.y < y && p2.y > y {
          points.push(Point::new(h_intersect(y, p1, p2), y));
       }
-      points.push(Point::new(p2.x, p2.y));
+      points.push(p2);
    }
 }
 
@@ -39,7 +39,7 @@ pub fn v_left_intersect(x: i64, p1: Point, p2: Point, points: &mut Vec<Point>) {
       if p1.x > x && p2.x < x {
          points.push(Point::new(x, v_intersect(x, p1, p2)));
       }
-      points.push(Point::new(p2.x, p2.y));
+      points.push(p2);
    }
 }
 
@@ -53,7 +53,7 @@ pub fn v_right_intersect(x: i64, p1: Point, p2: Point, points: &mut Vec<Point>) 
       if p1.x < x && p2.x > x {
          points.push(Point::new(x, v_intersect(x, p1, p2)));
       }
-      points.push(Point::new(p2.x, p2.y));
+      points.push(p2);
    }
 }
 
@@ -83,6 +83,82 @@ pub fn hv_clip<F>(intersect: F, y: i64, points: &mut Vec<Point>, start: usize) w
       intersect(y, p1, p2, points);
 
       p1 = p2;
+   }
+}
+
+pub fn h_split(y: i64, start: usize, down: &mut Vec<Point>, up: &mut Vec<Point>) {
+   let end = up.len();
+
+   assert!(end - start > 2);
+
+   let mut p1 = up[end-1];
+
+   for i in start..end {
+      let p2 = up[i];
+
+      h_split_edge(y, p1, p2, down, up);
+
+      p1 = p2;
+   }
+}
+
+pub fn v_split(x: i64, start: usize, left: &mut Vec<Point>, right: &mut Vec<Point>) {
+   let end = right.len();
+
+   assert!(end - start > 2);
+
+   let mut p1 = right[end-1];
+
+   for i in start..end {
+      let p2 = right[i];
+
+      v_split_edge(x, p1, p2, left, right);
+
+      p1 = p2;
+   }
+}
+
+#[inline]
+pub fn h_split_edge(y: i64, p1: Point, p2: Point, down: &mut Vec<Point>, up: &mut Vec<Point>) {
+   if p2.y > y {
+      if p1.y < y {
+         let intersection = Point::new(h_intersect(y, p1, p2), y);
+         down.push(intersection);
+         up.push(intersection);
+      }
+      up.push(p2);
+   } else if p2.y < y {
+      if p1.y > y {
+         let intersection = Point::new(h_intersect(y, p1, p2), y);
+         down.push(intersection);
+         up.push(intersection);
+      }
+      down.push(p2);
+   } else {
+      down.push(p2);
+      up.push(p2);
+   }
+}
+
+#[inline]
+pub fn v_split_edge(x: i64, p1: Point, p2: Point, left: &mut Vec<Point>, right: &mut Vec<Point>) {
+   if p2.x > x {
+      if p1.x < x {
+         let intersection = Point::new(x, v_intersect(x, p1, p2));
+         left.push(intersection);
+         right.push(intersection);
+      }
+      right.push(p2);
+   } else if p2.x < x {
+      if p1.x > x {
+         let intersection = Point::new(x, v_intersect(x, p1, p2));
+         left.push(intersection);
+         right.push(intersection);
+      }
+      left.push(p2);
+   } else {
+      left.push(p2);
+      right.push(p2);
    }
 }
 
