7348bf61c0acc847f378098dd99857fdbff58383
zahari <zahari@polydraw.com>
2015-11-17 00:40:35 +0200

    Tests for the circle intersection methods

--- src/geom/circle.rs
+++ src/geom/circle.rs
@@ -58,9 +58,73 @@ impl Circle {
    }
 
    #[inline]
-   fn other_delta(&self, other: i64) -> i64 {
-      assert!(other > 0);
-      assert!(other < self.radius);
-      (self.radius.pow(2) - other.pow(2)).sqrt()
+   fn other_delta(&self, delta: i64) -> i64 {
+      assert!(delta > 0);
+      assert!(delta < self.radius);
+      (self.radius.pow(2) - delta.pow(2)).sqrt()
+   }
+}
+
+
+#[cfg(test)]
+mod tests {
+   use super::super::point::Point;
+
+   use super::*;
+
+   #[test]
+   fn test_y_tr() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let y = circle.y_tr(120_000);
+      assert_eq!(y, 145_826);
+   }
+
+   #[test]
+   fn test_x_tr() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let x = circle.x_tr(120_000);
+      assert_eq!(x, 145_826);
+   }
+
+   #[test]
+   fn test_y_br() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let y = circle.y_br(120_000);
+      assert_eq!(y, 54_174);
+   }
+
+   #[test]
+   fn test_x_br() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let x = circle.x_br(60_000);
+      assert_eq!(x, 130_000);
+   }
+
+   #[test]
+   fn test_y_tl() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let y = circle.y_tl(60_000);
+      assert_eq!(y, 130_000);
+   }
+
+   #[test]
+   fn test_x_tl() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let x = circle.x_tl(120_000);
+      assert_eq!(x, 54_174);
+   }
+
+   #[test]
+   fn test_y_bl() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let y = circle.y_bl(60_000);
+      assert_eq!(y, 70_000);
+   }
+
+   #[test]
+   fn test_x_bl() {
+      let circle = Circle::new(Point::new(100_000, 100_000), 50_000);
+      let x = circle.x_bl(60_000);
+      assert_eq!(x, 70_000);
    }
 }
