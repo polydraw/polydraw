1f5168ea66b980c7ba6b47832efeb0d5b8d89186
zahari <zahari@polydraw.com>
2016-05-25 14:50:06 +0300

    Calculating the index of the point with min y

--- examples/rdev.rs
+++ examples/rdev.rs
@@ -42,6 +42,10 @@ impl DevRenderer {
    }
 
    fn _render_poly(&self, frame: &mut Frame, poly: &Poly) {
+      let min_y_index = _min_y_index(poly);
+
+      println!("[{:?}] = {:?}", min_y_index, poly.points[min_y_index]);
+
       for point in poly.points.iter() {
          frame.put_pixel(point.x as i32, point.y as i32, &poly.color)
       }
@@ -57,7 +61,33 @@ impl Renderer for DevRenderer {
       for poly in self.scene.polys.iter() {
          self._render_poly(frame, poly);
       }
+
+      panic!("Bye!");
    }
+}
+
+
+fn _min_y_index(poly: &Poly) -> usize {
+   let mut max_after = false;
+
+   let (first, rest) = poly.points.split_first().unwrap();
+
+   let mut min_y = first.y;
+   let mut min_index: usize = 0;
+
+   for (index, point) in rest.iter().enumerate() {
+      if point.y > min_y {
+         if max_after {
+            return min_index;
+         }
+      } else {
+         max_after = true;
+         min_index = index + 1;
+         min_y = point.y;
+      }
+   }
+
+   min_index
 }
 
 
@@ -67,19 +97,19 @@ fn main() {
 
    let poly_a = Poly {
       points: vec![
-         Point::new(200, 200),
          Point::new(1100, 400),
          Point::new(500, 700),
+         Point::new(200, 200),
       ],
       color: RGB::new(34, 78, 29),
    };
 
    let poly_b = Poly {
       points: vec![
-         Point::new(900, 200),
-         Point::new(900, 1000),
          Point::new(100, 500),
          Point::new(700, 100),
+         Point::new(900, 200),
+         Point::new(900, 1000),
       ],
       color: RGB::new(128, 59, 89),
    };
@@ -92,6 +122,7 @@ fn main() {
    Application::new()
       .renderer(&mut renderer)
       .title("Dev Rasterizer")
+      .size(800, 450)
       .run();
 }
 
