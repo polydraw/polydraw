b1a0e44b20eb7ef8c4da35ff04d9154f9367abc7
zahari <zahari@polydraw.com>
2016-05-25 13:15:17 +0300

    DevRenderer implementation started

--- examples/rdev.rs
+++ examples/rdev.rs
@@ -1,5 +1,6 @@
 extern crate polydraw;
 
+use polydraw::{Application, Renderer, Frame};
 use polydraw::geom::point::Point;
 use polydraw::draw::RGB;
 
@@ -10,15 +11,13 @@ struct Poly {
 }
 
 struct Scene {
-   background: RGB,
    polys: Vec<Poly>,
 }
 
 impl Scene {
    #[inline]
-   fn new(background: RGB) -> Self {
+   fn new() -> Self {
       Scene {
-         background: background,
          polys: Vec::new(),
       }
    }
@@ -30,26 +29,67 @@ impl Scene {
 }
 
 
+struct DevRenderer {
+   scene: Scene,
+}
+
+
+impl DevRenderer {
+   fn new(scene: Scene) -> Self {
+      DevRenderer {
+         scene: scene,
+      }
+   }
+}
+
+
+
+impl Renderer for DevRenderer {
+   fn render(&mut self, frame: &mut Frame) {
+      frame.clear();
+
+      let white = RGB::new(255, 255, 255);
+
+      for poly in self.scene.polys.iter() {
+         for point in poly.points.iter() {
+            frame.put_pixel(point.x as i32, point.y as i32, &white)
+         }
+      }
+   }
+}
+
+
+
 fn main() {
-   let mut scene = Scene::new(RGB::new(0, 0, 0));
+   let mut scene = Scene::new();
 
    let poly_a = Poly {
-      points: vec![Point::new(10, 10), Point::new(30, 20), Point::new(20, 30)],
+      points: vec![
+         Point::new(200, 200),
+         Point::new(1100, 400),
+         Point::new(500, 700),
+      ],
       color: RGB::new(34, 78, 29),
    };
 
    let poly_b = Poly {
-      points: vec![Point::new(40, 40), Point::new(5, 20), Point::new(50, 0)],
+      points: vec![
+         Point::new(900, 200),
+         Point::new(900, 1000),
+         Point::new(100, 500),
+         Point::new(700, 100),
+      ],
       color: RGB::new(128, 59, 89),
    };
 
    scene.push(poly_a);
    scene.push(poly_b);
 
-   println!("{:?}", scene.background);
+   let mut renderer = DevRenderer::new(scene);
 
-   for poly in scene.polys {
-      println!("{:?} - {:?}", poly.points, poly.color);
-   }
+   Application::new()
+      .renderer(&mut renderer)
+      .title("Dev Rasterizer")
+      .run();
 }
 
