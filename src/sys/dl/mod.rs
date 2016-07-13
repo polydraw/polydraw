7ac9fcef1d7def41798c34e62017da3d2d4596ea
zahari <zahari@polydraw.com>
2016-07-14 00:50:07 +0300

    Library struct wrapper around the Unix dynamic loading functions

--- src/sys/dl/mod.rs
+++ src/sys/dl/mod.rs
@@ -1,3 +1,49 @@
 #![cfg(target_os = "linux")]
 
 pub mod ffi;
+use std::ffi::CString;
+
+use error::{RuntimeError, ErrorKind};
+
+use super::utils::fn_ptr::FnPtr;
+
+pub struct Library {
+   pub handle: *mut ffi::c_void
+}
+
+impl Library {
+   pub fn new(name: &str) -> Result<Self, RuntimeError> {
+      let cname = try!(CString::new(name));
+
+      let handle = unsafe {
+         ffi::dlopen(cname.as_ptr(), ffi::RTLD_LAZY)
+      };
+
+      if handle.is_null() {
+         return Err(RuntimeError::new(
+            ErrorKind::DL,
+            format!("Loading dynamic library failed {}", name).to_string()
+         ));
+      }
+
+      Ok(Library {
+         handle: handle,
+      })
+   }
+
+   pub fn get(&self, name: &str) -> FnPtr {
+      let cname = CString::new(name).unwrap();
+
+      unsafe {
+         ffi::dlsym(self.handle, cname.as_ptr())
+      }
+   }
+}
+
+impl Drop for Library {
+   fn drop(&mut self) {
+      unsafe {
+         ffi::dlclose(self.handle)
+      };
+   }
+}
